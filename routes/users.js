affcf59cf5edb956bbf60a4c287abaefc1e13efb
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-01-29 07:39:37 +0100

    removing user route

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/user.controller');
-
-/* GET users listing. */
-router.post('/',controller.addData);
-
-module.exports = router;
